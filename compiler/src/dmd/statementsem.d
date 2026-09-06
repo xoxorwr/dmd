2775eec18f24267ade74a5e53c3da0187a261877
xoxorwr <alien.frog.apocalypse@gmail.com>
2026-09-06 12:14:13 +0200

    dot enum

--- compiler/src/dmd/statementsem.d
+++ compiler/src/dmd/statementsem.d
@@ -2293,6 +2293,8 @@ Statement statementSemanticVisit(Statement s, Scope* sc)
         bool errors = false;
 
         //printf("CaseStatement::semantic() %s\n", toChars());
+        if (sw && sw.condition && sw.condition.type)
+            cs.exp = inferExpType(cs.exp, sw.condition.type);
         sc = sc.startCTFE();
         cs.exp = cs.exp.expressionSemantic(sc);
         cs.exp = resolveProperties(sc, cs.exp);
